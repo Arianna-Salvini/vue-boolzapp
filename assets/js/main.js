ec03101934030f61c838fe1e9e50a80629c2c51d
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-14 10:33:37 +0100

    Create array of object for profiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
 console.log('hello');
+
+// ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+const {createApp} = Vue 
+
+createApp({
+    data(){
+        return {
+             profiles: [
+                {
+                    'image':'./assets/img/avatar_1jpg',
+                    'name':'Michele',
+                    'text':'Lorem ipsum',
+                    'time': 16.15,
+                },
+                {
+                    'image':'./assets/img/avatar_2jpg',
+                    'name':'Fabio',
+                    'text':'Lorem ipsum',
+                    'time': 16.15,
+                },
+                {
+                    'image':'./assets/img/avatar_3jpg',
+                    'name':'Samuele',
+                    'text':'Lorem ipsum',
+                    'time': 16.15,
+                },
+
+            ]
+
+        }
+    }
+    methods: {
+
+    }
+}).mount('#app')
